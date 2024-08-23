76cc2bf3f35b93b4d0c65d9d02e768124532ae02
Ryan Dsouza <r.dsouza@newmotion.com>
2024-08-23 09:53:08 +0100

    Initial commit

---
+++ test/lambda-s3-parser.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaS3Parser from '../lib/lambda-s3-parser-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-s3-parser-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaS3Parser.LambdaS3ParserStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
